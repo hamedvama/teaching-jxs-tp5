dc7cfea76ded36a4b47cd3b29b8602aadf9b326c
hamedvama <hamedbaba8@gmail.com>
2018-02-15 20:15:32 +0100

    first initialize object and use

--- js/pokedex.js
+++ js/pokedex.js
@@ -5,4 +5,19 @@ pokeApp.constant('POKEAPI', 'http://pokeapi.co');
 
 pokeApp.config(['$resourceProvider', function($resourceProvider) {
     $resourceProvider.defaults.stripTrailingSlashes = false;
-}]);
+}])
+
+<!-- Contrôleur avec une liste de pokemon initialisée pour le test -->
+pokeApp.controller('listpokem', ['$scope', '$log', function ($scope, $log) {
+    $scope.mypokemonlist = [{"id": "1", "name": "sangoku"},
+        {"id": "2", "name": "vegeta"},
+        {"id": "3", "name": "bougboug"},
+        {"id": "4", "name": "berus"},
+        {"id": "5", "name": "sangoten"}]
+
+    //apres le bouton ok
+    $scope.selection = function(){
+        $log.warn($scope.select);
+    }
+}])
+
