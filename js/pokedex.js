48cc0b035e18eae754809e181a25c793798e4916
hamedvama <hamedbaba8@gmail.com>
2018-02-15 20:45:38 +0100

    connection API and use $resources

--- js/pokedex.js
+++ js/pokedex.js
@@ -20,4 +20,23 @@ pokeApp.controller('listpokem', ['$scope', '$log', function ($scope, $log) {
         $log.warn($scope.select);
     }
 }])
+<!-- Resources permettant d'acceder a l'API pokemon -->
+pokeApp.factory('pokeresource', function ($resource) {
+    return $resource('https://pokeapi.co/api/v2/pokemon/:pokeid', {pokeid: '@id'},
+        {
+            queryAll: {
+                url: 'https://pokeapi.co/api/v2/pokemon/',
+                method: 'GET',
+                cache: false,
+                isArray: false
+            }
+        })
+})
+<!-- Contrôleur permettant de recuperer tous les pokemons en utilisant la ressource -->
+pokeApp.controller('listpokeappi', ['$scope', 'pokeresource', '$log', function ($scope, pokeresource, $log) {
 
+    pokeresource.queryAll().$promise.then(function (value) {
+        $scope.resultatresource = value.results;
+        $log.warn($scope.resultatresource);
+    });
+}]);
