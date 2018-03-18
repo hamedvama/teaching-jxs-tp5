e76fc448a74ab290ee604050d4534b1282aa297f
medkb <nafikbz@gmail.com>
2018-03-18 15:13:15 +0100

    Recherche d'un pokémon via son numéro

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,24 +1,31 @@
-var pokeApp = angular.module('POKEDEX', ['ngResource']);
+var pokeApp = angular.module('pokedex', ['ngResource']);
 
 // With this you can inject POKEAPI url wherever you want
-pokeApp.constant('POKEDEX', 'http://pokeapi.co');
+pokeApp.constant('POKEAPI', 'http://pokeapi.co');
 
 pokeApp.config(['$resourceProvider', function($resourceProvider) {
     $resourceProvider.defaults.stripTrailingSlashes = false;
-}])
+}]);
+
+pokeApp.controller('pokNum',['$scope','$log', function($scope,$log) {
+        {$log.log($scope.id);
+        };
+    }]);
 
 
 <!-- Contrôleur avec une liste de pokemon initialisée pour le test -->
+pokeApp.controller('listpokem', ['$scope', '$log', function ($scope, $log) {
+    $scope.mypokemonlist = [{"id": "1", "name": "sangoku"},
+        {"id": "2", "name": "vegeta"},
+        {"id": "3", "name": "bougboug"},
+        {"id": "4", "name": "berus"},
+        {"id": "5", "name": "sangoten"}]
 
-.controller('pokemonSearch',['$scope','$log', function($scope,$log) {
-    $scope.pokemons=[{id: '1', name: 'pidgeot'},{id:'2',name:'charmander'},{id:'3',name:'charizard'},{id:'4',name:'wartortle'},
-        {id:'5',name:'blastoise'},{id:'6',name:'butterfree'},{id: '7', name: 'pidgey'}];
-    $scope.logInfo = function()
-    {$log.log($scope.selection);
-        $log.info($scope.selection);
-    };
-}])
-
+    //apres le bouton ok
+    $scope.selection = function(){
+        $log.warn($scope.select);
+    }
+}]);
 <!-- Resources permettant d'acceder a l'API pokemon -->
 pokeApp.factory('pokeresource', function ($resource) {
     return $resource('https://pokeapi.co/api/v2/pokemon/:pokeid', {pokeid: '@id'},
@@ -30,7 +37,7 @@ pokeApp.factory('pokeresource', function ($resource) {
                 isArray: false
             }
         })
-})
+});
 <!-- Contrôleur permettant de recuperer tous les pokemons en utilisant la ressource -->
 pokeApp.controller('listpokeappi', ['$scope', 'pokeresource', '$log', function ($scope, pokeresource, $log) {
 
@@ -39,7 +46,7 @@ pokeApp.controller('listpokeappi', ['$scope', 'pokeresource', '$log', function (
         $log.warn($scope.resultatresource);
     });
 }])
-
+;
 pokeApp.controller('reqhttp', function ($scope, $http) {
     $http({
         method : "GET",
